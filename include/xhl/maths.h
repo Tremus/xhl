363bc8e37e01d3edff83221d16ffe4f4ee5680db
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2023-11-10 14:58:28 +1000

    Removed inlining. Added implementation half

--- include/xhl/maths.h
+++ include/xhl/maths.h
@@ -2,37 +2,94 @@
 #define XHL_MATHS_H
 #include <stdint.h>
 
-static inline int      xm_mini(int a, int b) { return a > b ? b : a; }
-static inline unsigned xm_minu(unsigned a, unsigned b) { return a > b ? b : a; }
-static inline int64_t  xm_minll(int64_t a, int64_t b) { return a > b ? b : a; }
-static inline uint64_t xm_minull(uint64_t a, uint64_t b) { return a > b ? b : a; }
-static inline float    xm_minf(float a, float b) { return a > b ? b : a; }
-static inline double   xm_mind(double a, double b) { return a > b ? b : a; }
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-static inline int      xm_maxi(int a, int b) { return a < b ? b : a; }
-static inline unsigned xm_maxu(unsigned a, unsigned b) { return a < b ? b : a; }
-static inline int64_t  xm_maxll(int64_t a, int64_t b) { return a < b ? b : a; }
-static inline uint64_t xm_maxull(uint64_t a, uint64_t b) { return a < b ? b : a; }
-static inline float    xm_maxf(float a, float b) { return a < b ? b : a; }
-static inline double   xm_maxd(double a, double b) { return a < b ? b : a; }
+int      xm_mini(int a, int b);
+unsigned xm_minu(unsigned a, unsigned b);
+int64_t  xm_minll(int64_t a, int64_t b);
+uint64_t xm_minull(uint64_t a, uint64_t b);
+float    xm_minf(float a, float b);
+double   xm_mind(double a, double b);
 
-static inline int      xm_clampi(int v, int lo, int hi) { return v < lo ? lo : (hi < v ? hi : v); }
-static inline unsigned xm_clampu(unsigned v, unsigned lo, unsigned hi) { return v < lo ? lo : (hi < v ? hi : v); }
-static inline int64_t  xm_clampll(int64_t v, int64_t lo, int64_t hi) { return v < lo ? lo : (hi < v ? hi : v); }
-static inline uint64_t xm_clampull(uint64_t v, uint64_t lo, uint64_t hi) { return v < lo ? lo : (hi < v ? hi : v); }
-static inline float    xm_clampf(float v, float lo, float hi) { return v < lo ? lo : (hi < v ? hi : v); }
-static inline double   xm_clampd(double v, double lo, double hi) { return v < lo ? lo : (hi < v ? hi : v); }
+int      xm_maxi(int a, int b);
+unsigned xm_maxu(unsigned a, unsigned b);
+int64_t  xm_maxll(int64_t a, int64_t b);
+uint64_t xm_maxull(uint64_t a, uint64_t b);
+float    xm_maxf(float a, float b);
+double   xm_maxd(double a, double b);
+
+int      xm_clampi(int v, int lo, int hi);
+unsigned xm_clampu(unsigned v, unsigned lo, unsigned hi);
+int64_t  xm_clampll(int64_t v, int64_t lo, int64_t hi);
+uint64_t xm_clampull(uint64_t v, uint64_t lo, uint64_t hi);
+float    xm_clampf(float v, float lo, float hi);
+double   xm_clampd(double v, double lo, double hi);
 
 // Integer linear interpolation requires numerator over denominator
-static inline int xm_lerpi(int num, int denom, int start, int end) { return start + (num * (end - start)) / denom; }
-static inline unsigned xm_lerpu(unsigned nu, unsigned de, unsigned s, unsigned e) { return s + (nu * (e - s)) / de; }
-static inline int64_t  xm_lerpll(int64_t num, int64_t den, int64_t s, int64_t e) { return s + (num * (e - s)) / den; }
-static inline uint64_t xm_lerpull(uint64_t n, uint64_t d, uint64_t s, uint64_t e) { return s + (n * (e - s)) / d; }
-static inline float    xm_lerpf(float frac, float start, float end) { return start + frac * (end - start); }
-static inline double   xm_lerpd(double frac, double start, double end) { return start + frac * (end - start); }
+int      xm_lerpi(int num, int denom, int start, int end);
+unsigned xm_lerpu(unsigned nu, unsigned de, unsigned s, unsigned e);
+int64_t  xm_lerpll(int64_t num, int64_t den, int64_t s, int64_t e);
+uint64_t xm_lerpull(uint64_t n, uint64_t d, uint64_t s, uint64_t e);
+float    xm_lerpf(float frac, float start, float end);
+double   xm_lerpd(double frac, double start, double end);
+
+float  xm_mapf(float v, float start1, float end1, float start2, float end2);
+double xm_mapd(double v, double start1, double end1, double start2, double end2);
+
+float  xm_normf(float v, float start, float end);
+double xm_normd(double v, double start, double end);
+
+uint32_t xm_xorshift32(uint32_t x);
+uint64_t xm_xorshift64(uint32_t x);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // XHL_MATHS_H
+
+#ifdef XHL_MATHS_IMPL
+#undef XHL_MATHS_IMPL
+
+int      xm_mini(int a, int b) { return a > b ? b : a; }
+unsigned xm_minu(unsigned a, unsigned b) { return a > b ? b : a; }
+int64_t  xm_minll(int64_t a, int64_t b) { return a > b ? b : a; }
+uint64_t xm_minull(uint64_t a, uint64_t b) { return a > b ? b : a; }
+float    xm_minf(float a, float b) { return a > b ? b : a; }
+double   xm_mind(double a, double b) { return a > b ? b : a; }
+
+int      xm_maxi(int a, int b) { return a < b ? b : a; }
+unsigned xm_maxu(unsigned a, unsigned b) { return a < b ? b : a; }
+int64_t  xm_maxll(int64_t a, int64_t b) { return a < b ? b : a; }
+uint64_t xm_maxull(uint64_t a, uint64_t b) { return a < b ? b : a; }
+float    xm_maxf(float a, float b) { return a < b ? b : a; }
+double   xm_maxd(double a, double b) { return a < b ? b : a; }
+
+int      xm_clampi(int v, int lo, int hi) { return v < lo ? lo : (hi < v ? hi : v); }
+unsigned xm_clampu(unsigned v, unsigned lo, unsigned hi) { return v < lo ? lo : (hi < v ? hi : v); }
+int64_t  xm_clampll(int64_t v, int64_t lo, int64_t hi) { return v < lo ? lo : (hi < v ? hi : v); }
+uint64_t xm_clampull(uint64_t v, uint64_t lo, uint64_t hi) { return v < lo ? lo : (hi < v ? hi : v); }
+float    xm_clampf(float v, float lo, float hi) { return v < lo ? lo : (hi < v ? hi : v); }
+double   xm_clampd(double v, double lo, double hi) { return v < lo ? lo : (hi < v ? hi : v); }
+
+// Integer linear interpolation requires numerator over denominator
+int      xm_lerpi(int num, int denom, int start, int end) { return start + (num * (end - start)) / denom; }
+unsigned xm_lerpu(unsigned nu, unsigned de, unsigned s, unsigned e) { return s + (nu * (e - s)) / de; }
+int64_t  xm_lerpll(int64_t num, int64_t den, int64_t s, int64_t e) { return s + (num * (e - s)) / den; }
+uint64_t xm_lerpull(uint64_t n, uint64_t d, uint64_t s, uint64_t e) { return s + (n * (e - s)) / d; }
+float    xm_lerpf(float frac, float start, float end) { return start + frac * (end - start); }
+double   xm_lerpd(double frac, double start, double end) { return start + frac * (end - start); }
+
+float  xm_mapf(float v, float s1, float e1, float s2, float e2) { return s2 + ((e2 - s2) * (v - s1)) / (e1 - s1); }
+double xm_mapd(double v, double s1, double e1, double s2, double e2) { return s2 + ((e2 - s2) * (v - s1)) / (e1 - s1); }
+
+float  xm_normf(float v, float start, float end) { return (v - start) / (end - start); }
+double xm_normd(double v, double start, double end) { return (v - start) / (end - start); }
 
 // https://en.wikipedia.org/wiki/Xorshift
-static inline uint32_t xm_xorshift32(uint32_t x)
+uint32_t xm_xorshift32(uint32_t x)
 {
     /* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
     x ^= x << 13;
@@ -40,7 +97,7 @@ static inline uint32_t xm_xorshift32(uint32_t x)
     x ^= x << 5;
     return x;
 }
-static inline uint64_t xm_xorshift64(uint32_t x)
+uint64_t xm_xorshift64(uint32_t x)
 {
     x ^= x << 13;
     x ^= x >> 7;
@@ -48,4 +105,4 @@ static inline uint64_t xm_xorshift64(uint32_t x)
     return x;
 }
 
-#endif // XHL_MATHS_H
+#endif // XHL_MATHS_IMPL
