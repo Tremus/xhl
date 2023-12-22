9caf7858ca9340c81cac1a370a28a7372243ee9a
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2023-12-22 13:39:51 +1000

    better print on windows

--- include/xhl/debug.h
+++ include/xhl/debug.h
@@ -15,12 +15,16 @@
 
 #else // NDEBUG
 #define XHL_DEBUG_BREAK
-#define xassert(cond)                                                                                                  \
-    do                                                                                                                 \
-    {                                                                                                                  \
-        (void)(cond);                                                                                                  \
-    }                                                                                                                  \
-    while (0)
+// clang-format off
+#define xassert(cond) do { (void)(cond); } while (0)
+// clang-format on
+#endif
+
+#ifdef _WIN32
+// Prints to VSCode debug console properly
+#define xprintf(str, ...) (printf(str, __VA_ARGS__), fflush(stdout))
+#else
+#define xprintf printf
 #endif
 
 #if defined(_MSC_VER) && ! defined(__clang__)
@@ -29,48 +33,4 @@
 #define xstatic_assert(cond, msg) _Static_assert(cond, msg)
 #endif
 
-#ifdef __cplusplus
-#define XHL_DEBUG_RESTRICT
-extern "C" {
-#else
-#define XHL_DEBUG_RESTRICT restrict
-#endif
-
-#ifdef _WIN32
-// Prints to VSCode debug console properly, unlike regular printf...
-void xprintf(const char* XHL_DEBUG_RESTRICT fmt, ...);
-#else
-#define xprintf printf
-#endif
-
-#ifdef __cplusplus
-}
-#endif
 #endif // XHL_DEBUG_H
-
-#ifdef XHL_DEBUG_IMPL
-#undef XHL_DEBUG_IMPL
-
-#ifndef _DEBUG
-#error "Don't print in release!"
-#endif
-
-#ifdef _WIN32
-#include <stdarg.h>
-#include <windows.h>
-
-void xprintf(const char* XHL_DEBUG_RESTRICT fmt, ...)
-{
-    va_list args;
-    char    buf[256];
-
-    va_start(args, fmt);
-    vsnprintf(buf, sizeof(buf), fmt, args);
-
-    OutputDebugStringA(buf);
-
-    va_end(args);
-}
-#endif
-
-#endif // XHL_DEBUG_IMPL
